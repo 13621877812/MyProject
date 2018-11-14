8a2cfe78fe751954053c5d39bda58a24ed8aae9f
sun <1549057151@qq.com>
2018-11-14 22:47:26 +0800

    1

---
+++ AppDelegate.swift
@@ -0,0 +1,115 @@
+
+//
+//  AppDelegate.swift
+//  OrderMenu
+//
+//  Created by  on 2018/7/17.
+//  Copyright © 2018年 . All rights reserved.
+//
+
+import UIKit
+import CoreData
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+       
+        MagicalRecord.setupCoreDataStack(withAutoMigratingSqliteStoreNamed:MagicalRecord.defaultStoreName())
+        
+        
+        
+
+
+        // Override point for customization after application launch.
+        return true
+    }
+
+   
+   
+    func initIQKeyboardManager(){
+        let keyboardManager:IQKeyboardManager = IQKeyboardManager.shared()
+        keyboardManager.isEnabled = true
+        keyboardManager.shouldResignOnTouchOutside = true; // 控制点击背景是否收起键盘
+        keyboardManager.shouldToolbarUsesTextFieldTintColor = true; // 控制键盘上的工具条文字颜色是否用户自定义
+        keyboardManager.toolbarManageBehaviour = .bySubviews; // 有多个输入框时，可以通过点击Toolbar 上的“前一个”“后一个”按钮来实现移动到不同的输入框
+        keyboardManager.isEnableAutoToolbar = true; // 控制是否显示键盘上的工具条
+        keyboardManager.shouldShowToolbarPlaceholder = true; // 是否显示占位文字
+        //    keyboardManager.placeholderFont = [UIFont boldSystemFontOfSize:17]; // 设置占位文字的字体
+        keyboardManager.keyboardDistanceFromTextField = 10.0 // 输入框距离键盘的距离
+        keyboardManager.toolbarDoneBarButtonItemText = "完成"
+        keyboardManager.toolbarPreviousNextAllowedClasses.add(IQPreviousNextView.superclass())
+    }
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        // Saves changes in the application's managed object context before the application terminates.
+        self.saveContext()
+    }
+
+    // MARK: - Core Data stack
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+        */
+        let container = NSPersistentContainer(name: "OrderMenu")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                 
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
+}
+
